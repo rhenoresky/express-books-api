f9c9aa6901d4681695d78b7739a4943ac194ed31
rhenoresky <rheno2002@gmail.com>
2024-01-20 18:35:25 +0700

    change port

--- src/server.js
+++ src/server.js
@@ -7,7 +7,7 @@ import authRouter from './auth/controller.js';
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 
 app.use(express.json());
 app.use('/categories', categoriesRouter);
