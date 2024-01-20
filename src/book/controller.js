a76071bbeb50aec3353e900a29f58b7debae285a
rhenoresky <rheno2002@gmail.com>
2024-01-20 12:16:18 +0700

    feat: book

---
+++ src/book/controller.js
@@ -0,0 +1,57 @@
+import express from 'express';
+import {getBooks, postBook, updateBook, deleteBook} from './service.js';
+const router = express.Router();
+
+router.get('/', async (req, res) => {
+  const result = await getBooks(req.body);
+
+  res.json(result);
+});
+
+router.post('/', async (req, res) => {
+  if (req.body.release_year < 1980 || req.body.release_year > 2021) {
+    return res.status(400).json({message: 'wrong release_year'});
+  }
+
+  if (req.body.total_page > 0 && req.body.total_page <= 100) {
+    req.body.thickness = 'tipis';
+  } else if (req.body.total_page >= 101 && req.body.total_page <= 200) {
+    req.body.thickness = 'sedang';
+  } else if (req.body.total_page >= 201) {
+    req.body.thickness = 'tebal';
+  } else if (req.body.total_page <= 0) {
+    return res.status(400).json({message: 'wrong total_page'});
+  }
+
+  await postBook(req.body);
+
+  res.status(201).json({message: 'add book successfully'});
+});
+
+router.patch('/:id', async (req, res) => {
+  if (req.body.release_year < 1980 || req.body.release_year > 2021) {
+    return res.status(400).json({message: 'wrong release_year'});
+  }
+
+  if (req.body.total_page > 0 && req.body.total_page <= 100) {
+    req.body.thickness = 'tipis';
+  } else if (req.body.total_page >= 101 && req.body.total_page <= 200) {
+    req.body.thickness = 'sedang';
+  } else if (req.body.total_page >= 201) {
+    req.body.thickness = 'tebal';
+  } else if (req.body.total_page <= 0) {
+    return res.status(400).json({message: 'wrong total_page'});
+  }
+
+  await updateBook(parseInt(req.params.id), req.body);
+
+  res.json({message: 'edit book successfully'});
+});
+
+router.delete('/:id', async (req, res) => {
+  await deleteBook(parseInt(req.params.id));
+
+  res.json({message: 'delete book successfully'});
+});
+
+export default router;
---
+++ src/book/service.js
@@ -0,0 +1,37 @@
+import {PrismaClient} from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+const getBooks = () => {
+  return prisma.book.findMany();
+};
+
+const postBook = async (body) => {
+  await prisma.book.create({
+    data: {
+      ...body,
+    },
+  });
+};
+
+const updateBook = async (...data) => {
+  const [id, body] = data;
+  await prisma.book.update({
+    where: {
+      id,
+    },
+    data: {
+      ...body,
+    },
+  });
+};
+
+const deleteBook = async (id) => {
+  await prisma.book.delete({
+    where: {
+      id,
+    },
+  });
+};
+
+export {getBooks, postBook, updateBook, deleteBook};
